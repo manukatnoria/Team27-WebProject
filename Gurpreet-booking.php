758202763026261b947581b93fc7f7c83a79d54d
gopigalib <143167146+gopigalib@users.noreply.github.com>
2024-02-15 12:23:38 +0200

    edit

--- Gurpreet-booking.php
+++ Gurpreet-booking.php
@@ -79,8 +79,7 @@ if (isset($_POST['submit'])) {
     <input type="submit" value="Submit!!" name="submit">
 
 <!-- Edit Your Response button -->
-<a href="Gurpreet-booking.php" class="edit-response-button">Edit Your Response</a>
-</form>
+<a href="form/edit_booking.php" class="edit-response-button">Edit Your Response</a>
 
 <?php
 include("footer.php");
